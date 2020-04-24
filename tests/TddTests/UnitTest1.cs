6a8116e80540f5c878fa83073934285a539fda58
leonardo.silva <leonardo.silva@hbsis.com.br>
2020-04-24 14:55:36 -0300

    teste retorno 0

--- tests/TddTests/UnitTest1.cs
+++ tests/TddTests/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace TddTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Deve_retornar_zero_quando_o_input_for_vazio()
         {
             var calculadora = new StringCalculator();
 
@@ -15,13 +15,23 @@ namespace TddTests
 
             soma.Should().Be(0);
         }
+
+        [Fact]
+        public void Deve_retornar_o_mesmo_numero_se_ele_vier_sozinho()
+        {
+            var calculadora = new StringCalculator();
+
+            int soma = calculadora.Adicionar("1");
+
+            soma.Should().Be(1);
+        }
     }
 
     public class StringCalculator
     {
         public int Adicionar(string numeros)
         {
-            throw new NotImplementedException();
+            return 0;
         }
     }
 }
